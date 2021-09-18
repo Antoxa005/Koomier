fdaf1a6c6e9b09c42e8ac98768c2c2f86b3a5cb9
Antoxa005 <66873872+Antoxa005@users.noreply.github.com>
2021-09-19 02:27:05 +0300

    blablabla

---
+++ Bot.py
@@ -0,0 +1,168 @@
+import pygame
+
+class Bot:
+    def __init__(self, startX, startY, width, height, tileWidth, tileHeight, color, listOfTiles):
+        self.rect = pygame.Rect(startX, startY, width, height)
+
+        self.tileWidth = tileWidth
+        self.tileHeight = tileHeight
+
+        self.listOfTiles = listOfTiles
+
+        #self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), width, height)
+        self.color = color
+
+    def FindTile(self):
+        for tile in self.listOfTiles:
+            if tile.rect.centerx == self.rect.centerx and tile.rect.centery == self.rect.centery:
+                return tile
+
+    def Down(self):
+        self.rect.y += self.tileHeight
+    def Up(self):
+        self.rect.y -= self.tileHeight
+    def Left(self):
+        self.rect.x -= self.tileWidth
+    def Right(self):
+        self.rect.x += self.tileWidth
+
+    def Color(self):
+        self.FindTile().Color((255, 0, 0))
+
+    def DeColor(self):
+        self.FindTile().DeColor()
+
+class Tile:
+    def __init__(self, x, y, width, height, color, wallNorth=False, wallSouth=False, wallWest=False, wallEast=False, needsToBeColored=False, isColored=False, startPoint=False, endPoint=False):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.ogColor = color
+
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+
+
+        self.image = pygame.Surface((width, height))
+        self.image.fill(self.color)
+        self.walls = [wallNorth, wallSouth, wallWest, wallEast]
+
+        self.wallNorth = wallNorth
+        self.wallSouth = wallSouth
+        self.wallWest = wallWest
+        self.wallEast = wallEast
+
+        self.needsToBeColored = needsToBeColored
+        self.isColored = isColored
+
+
+    def Color(self, newColor):
+        self.isColored = True
+        self.color = newColor
+
+        self.image.fill(self.color)
+
+    def DeColor(self):
+        self.isColored = False
+        self.color = self.ogColor
+
+        self.image.fill(self.color)
+
+
+pygame.init()
+info = pygame.display.Info()
+screenWidth, screenHeight = info.current_w, info.current_h
+window = pygame.display.set_mode((screenWidth, screenHeight), pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+def UpdateTiles(listOfTiles):
+    for tile in listOfTiles:
+
+        pygame.draw.rect(window, tile.color, tile.rect)
+
+        if tile.wallNorth:
+            pygame.draw.line(window, (0, 0, 150), (tile.rect.x, tile.rect.y), (tile.rect.x + tile.rect.width, tile.rect.y), 5)
+
+        else:
+            pygame.draw.line(window, (255, 255, 0), (tile.rect.x, tile.rect.y), (tile.rect.x + tile.rect.width, tile.rect.y), 5)
+
+        if tile.wallSouth:
+            pygame.draw.line(window, (0, 0, 150), (tile.rect.x, tile.rect.y + tile.rect.height), (tile.rect.x + tile.rect.width, tile.rect.y + tile.rect.height), 5)
+
+        else:
+            pygame.draw.line(window, (255, 255, 0), (tile.rect.x, tile.rect.y + tile.rect.height), (tile.rect.x + tile.rect.width, tile.rect.y + tile.rect.height), 5)
+
+        if tile.wallWest:
+            pygame.draw.line(window, (0, 0, 150), (tile.rect.x, tile.rect.y), (tile.rect.x, tile.rect.y + tile.rect.height), 5)
+
+        else:
+            pygame.draw.line(window, (255, 255, 0), (tile.rect.x, tile.rect.y), (tile.rect.x, tile.rect.y + tile.rect.height), 5)
+
+        if tile.wallEast:
+            pygame.draw.line(window, (0, 0, 150), (tile.rect.x + tile.rect.width, tile.rect.y), (tile.rect.x + tile.rect.width, tile.rect.y + tile.rect.height), 5)
+
+        else:
+            pygame.draw.line(window, (255, 255, 0), (tile.rect.x + tile.rect.width, tile.rect.y), (tile.rect.x + tile.rect.width, tile.rect.y + tile.rect.height), 5)
+
+listOfTiles = []
+for x in range(0, 640, 80):
+    for y in range(0, 640, 80):
+        tile = Tile(x, y, 80, 80, (0, 255, 0), False, False, False, False, False, False, False, False)
+        listOfTiles.append(tile)
+bot = Bot(20, 20, 40, 40, 80, 80, (0, 0, 0), listOfTiles)
+run = True
+ansCode = []
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                run = False
+            if keys[pygame.K_w]:
+                ansCode.append("bot.Up()")
+                bot.rect.y -= 80
+            elif keys[pygame.K_s]:
+                ansCode.append("bot.Down()")
+                bot.rect.y += 80
+            elif keys[pygame.K_a]:
+                ansCode.append("bot.Left()")
+                bot.rect.x -= 80
+            elif keys[pygame.K_d]:
+                ansCode.append("bot.Right()")
+                bot.rect.x += 80
+
+            if keys[pygame.K_SPACE]:
+                if bot.FindTile().isColored == True:
+                    ansCode.append("bot.DeColor()")
+                    bot.DeColor()
+                else:
+                    ansCode.append("bot.Color()")
+                    bot.Color()
+
+            if keys[pygame.K_p]:
+                print("--------------------------------------")
+                for ans in ansCode:
+                    print(ans)
+                ansCode.clear()
+
+            if keys[pygame.K_r]:
+                bot.rect.x, bot.rect.y = 20, 20
+                ansCode.clear()
+
+                for tile in listOfTiles:
+                    tile.DeColor()
+
+
+    window.fill((255, 255, 255))
+
+    UpdateTiles(listOfTiles)
+    pygame.draw.rect(window, bot.color, bot.rect)
+
+    pygame.display.update()
+pygame.quit()
