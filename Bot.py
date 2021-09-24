d9890cd4afeaa8aad1ec8697b88303cefbc784ea
Antoxa005 <66873872+Antoxa005@users.noreply.github.com>
2021-09-25 01:06:15 +0300

    Update

--- Bot.py
+++ Bot.py
@@ -8,56 +8,131 @@ class Bot:
         self.tileWidth = tileWidth
         self.tileHeight = tileHeight
 
+        self.ogX = startX
+        self.ogY = startY
+
+        self.path = []
+
         self.listOfTiles = listOfTiles
 
         #self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), width, height)
         self.color = color
+        self.ogColor = color
+        self.brokenColor = (255, 0, 0)
 
         self.broken = False
         self.movementEnabled = False
 
-    def FindTile(self):
+    def MoveFindTile(self):
         for tile in self.listOfTiles:
             if tile.rect.centerx == self.rect.centerx and tile.rect.centery == self.rect.centery:
                 return tile
 
-    def Down(self):
-        if not self.FindTile().wallSouth:
+    def MoveDown(self):
+        if not self.MoveFindTile().wallSouth:
             self.rect.y += 80
-            if self.FindTile() != None:
-                if self.FindTile().wallNorth:
+            if self.MoveFindTile() != None:
+                if self.MoveFindTile().wallNorth:
                     self.rect.y -= 80
             else:
                 self.rect.y -= 80
-    def Up(self):
-        if not self.FindTile().wallNorth:
+    def MoveUp(self):
+        if not self.MoveFindTile().wallNorth:
             self.rect.y -= 80
-            if self.FindTile() != None:
-                if self.FindTile().wallSouth:
+            if self.MoveFindTile() != None:
+                if self.MoveFindTile().wallSouth:
                     self.rect.y += 80
             else:
                 self.rect.y += 80
-    def Left(self):
-        if not self.FindTile().wallWest:
+    def MoveLeft(self):
+        if not self.MoveFindTile().wallWest:
             self.rect.x -= 80
-            if self.FindTile() != None:
-                if self.FindTile().wallEast:
+            if self.MoveFindTile() != None:
+                if self.MoveFindTile().wallEast:
                     self.rect.x += 80
             else:
                 self.rect.x += 80
-    def Right(self):
-        if not self.FindTile().wallEast:
+    def MoveRight(self):
+        if not self.MoveFindTile().wallEast:
             self.rect.x += 80
-            if self.FindTile() != None:
-                if self.FindTile().wallWest:
+            if self.MoveFindTile() != None:
+                if self.MoveFindTile().wallWest:
                     self.rect.x -= 80
             else:
                 self.rect.x -= 80
+
+    #--------------------------------------------------------
+
+    def FindTile(self):
+        for tile in self.listOfTiles:
+            if tile.rect.centerx == self.newRect.centerx and tile.rect.centery == self.newRect.centery:
+                return tile
+
+    def Down(self):
+        if self.broken == False:
+            if not self.FindTile().wallSouth:
+                self.newRect.y += 80
+                if self.FindTile() != None:
+                    if self.FindTile().wallNorth:
+                        self.newRect.y -= 80
+                        self.broken = True
+                else:
+                    self.newRect.y -= 80
+                    self.broken = True
+            else:
+                self.broken = True
+            self.path.append((self.newRect.x, self.newRect.y))
+
+    def Up(self):
+        if self.broken == False:
+            if not self.FindTile().wallNorth:
+                self.newRect.y -= 80
+                if self.FindTile() != None:
+                    if self.FindTile().wallSouth:
+                        self.newRect.y += 80
+                        self.broken = True
+                else:
+                    self.newRect.y += 80
+                    self.broken = True
+            else:
+                self.broken = True
+            self.path.append((self.newRect.x, self.newRect.y))
+
+    def Left(self):
+        if self.broken == False:
+            if not self.FindTile().wallWest:
+                self.newRect.x -= 80
+                if self.FindTile() != None:
+                    if self.FindTile().wallEast:
+                        self.newRect.x += 80
+                        self.broken = True
+                else:
+                    self.newRect.x += 80
+                    self.broken = True
+            else:
+                self.broken = True
+            self.path.append((self.newRect.x, self.newRect.y))
+
+    def Right(self):
+        if self.broken == False:
+            if not self.FindTile().wallEast:
+                self.newRect.x += 80
+                if self.FindTile() != None:
+                    if self.FindTile().wallWest:
+                        self.newRect.x -= 80
+                        self.broken = True
+                else:
+                    self.newRect.x -= 80
+                    self.broken = True
+            else:
+                self.broken = True
+            self.path.append((self.newRect.x, self.newRect.y))
+
     def Color(self):
-        self.FindTile().Color((255, 0, 0))
+        self.MoveFindTile().Color((255, 0, 0))
 
     def DeColor(self):
-        self.FindTile().DeColor()
+        self.MoveFindTile().DeColor()
 
 class Tile:
     def __init__(self, x, y, width, height, color, wallNorth=False, wallSouth=False, wallWest=False, wallEast=False, needsToBeColored=False, isColored=False, startPoint=False, endPoint=False):
@@ -135,7 +210,8 @@ def UpdateTiles(listOfTiles):
             pygame.draw.line(window, (255, 255, 0), (tile.rect.x + tile.rect.width, tile.rect.y), (tile.rect.x + tile.rect.width, tile.rect.y + tile.rect.height), 5)
 
 def ResetButtonClick():
-    bot.rect.x, bot.rect.y = 20, 20
+    bot.rect.x, bot.rect.y = bot.ogX, bot.ogY
+    bot.color = bot.ogColor
     ansCode.clear()
     for tile in listOfTiles:
         tile.DeColor()
@@ -150,8 +226,25 @@ def ToggleMoveButtonClick():
     else:
         bot.movementEnabled = True
 def TeleportButtonClick():
-    bot.rect = bot.newRect
-    fgfhbhnjj = 1
+    bot.rect.x = bot.newRect.x
+    bot.rect.y = bot.newRect.y
+def StepByStepButtonClick():
+    n = 0
+    for i in bot.path:
+        pygame.time.wait(100)
+        bot.rect.x = i[0]
+        bot.rect.y = i[1]
+        window.fill((255, 255, 255))
+
+        UpdateTiles(listOfTiles)
+        pygame.draw.rect(window, bot.color, bot.rect)
+
+        for button in listOfButtons:
+            pygame.draw.rect(window, button.color, button.rect)
+        pygame.display.update()
+    if bot.broken:
+        bot.color = bot.brokenColor
+        print("ERROR: BOT BROKEN")
 
 listOfTiles = []
 for x in range(640, -80, -80):
@@ -161,12 +254,27 @@ for x in range(640, -80, -80):
 bot = Bot(20, 20, 40, 40, 80, 80, (0, 0, 0), listOfTiles)
 run = True
 ansCode = []
+clock = pygame.time.Clock()
 
 ResetButton = Button(800, 10, 80, 80, ResetButtonClick, (0, 0, 0))
 PrintButton = Button(900, 10, 80, 80, PrintButtonClick, (100, 180, 0))
 ToggleMoveButton = Button(1000, 10, 80, 80, ToggleMoveButtonClick, (50, 50, 125))
 TeleportButton = Button(1100, 10, 80, 80, TeleportButtonClick, (255, 0, 255))
-listOfButtons = [ResetButton, PrintButton, ToggleMoveButton, TeleportButton]
+StepByStepButton = Button(1200, 10, 80, 80, StepByStepButtonClick, (100, 0, 0))
+listOfButtons = [ResetButton, PrintButton, ToggleMoveButton, TeleportButton, StepByStepButton]
+
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Down()
+bot.Up()
+bot.Down()
+
 
 for tile in listOfTiles:
     if tile.rect.x == 0:
@@ -178,7 +286,10 @@ for tile in listOfTiles:
     if tile.rect.y == 640:
         tile.wallSouth = True
 
+timeOld = pygame.time.get_ticks()
 while run:
+    clock.tick(60)
+    timeNew = pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -188,35 +299,6 @@ while run:
             if keys[pygame.K_ESCAPE]:
                 run = False
 
-            if bot.movementEnabled:
-                if keys[pygame.K_w] and bot.FindTile().wallNorth == False:
-                    ansCode.append("bot.Up()")
-                    bot.rect.y -= 80
-                    if bot.FindTile().wallSouth:
-                        bot.rect.y += 80
-                elif keys[pygame.K_s] and bot.FindTile().wallSouth == False:
-                    ansCode.append("bot.Down()")
-                    bot.rect.y += 80
-                    if bot.FindTile().wallNorth:
-                        bot.rect.y += 80
-                elif keys[pygame.K_a] and bot.FindTile().wallWest == False:
-                    ansCode.append("bot.Left()")
-                    bot.rect.x -= 80
-                    if bot.FindTile().wallEast:
-                        bot.rect.x += 80
-                elif keys[pygame.K_d] and bot.FindTile().wallEast == False:
-                    ansCode.append("bot.Right()")
-                    bot.rect.x += 80
-                    if bot.FindTile().wallWest:
-                        bot.rect.x -= 80
-                if keys[pygame.K_SPACE]:
-                    if bot.FindTile().isColored == True:
-                        ansCode.append("bot.DeColor()")
-                        bot.DeColor()
-                    else:
-                        ansCode.append("bot.Color()")
-                        bot.Color()
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             left, middle, right = pygame.mouse.get_pressed()
 
@@ -225,7 +307,31 @@ while run:
                 for button in listOfButtons:
                     button.OnClick(mousePos)
 
+    keys = pygame.key.get_pressed()
     mousePos = pygame.mouse.get_pos()
+    if bot.movementEnabled:
+        if timeNew - timeOld >= 250:
+            if keys[pygame.K_w]:
+                ansCode.append("bot.Up()")
+                bot.MoveUp()
+            elif keys[pygame.K_s]:
+                ansCode.append("bot.Down()")
+                bot.MoveDown()
+            elif keys[pygame.K_a]:
+                ansCode.append("bot.Left()")
+                bot.MoveLeft()
+            elif keys[pygame.K_d]:
+                ansCode.append("bot.Right()")
+                bot.MoveRight()
+            timeOld = pygame.time.get_ticks()
+
+        if keys[pygame.K_SPACE]:
+            if bot.FindTile().isColored == True:
+                ansCode.append("bot.DeColor()")
+                bot.DeColor()
+            else:
+                ansCode.append("bot.Color()")
+                bot.Color()
 
     for button in listOfButtons:
         if button.rect.collidepoint(mousePos):
@@ -240,6 +346,8 @@ while run:
 
     for button in listOfButtons:
         pygame.draw.rect(window, button.color, button.rect)
+
+    pygame.display.set_caption(str(clock.get_fps()))
 
     pygame.display.update()
 pygame.quit()
